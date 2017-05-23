d437f525e601c3b1595dddbb8c898b15ae1514a6
Markus Werdien <markuswerdien@hotmail.com>
2017-05-23 17:45:15 +0200

    Made a .dds texture and hardcoded a cube to draw sky on

--- Egyptian_Horror_Story/Egyptian_Horror_Story/SkyRenderer.cpp
+++ Egyptian_Horror_Story/Egyptian_Horror_Story/SkyRenderer.cpp
@@ -9,11 +9,14 @@ SkyRenderer::SkyRenderer()
 
 SkyRenderer::~SkyRenderer()
 {
-	this->srv->Release();
+	if (this->srv)
+		this->srv->Release();
 }
 
 void SkyRenderer::setup(ID3D11Device* device, ShaderHandler& shaderHandler)
 {
+	using namespace DirectX::SimpleMath;
+
 	D3D11_INPUT_ELEMENT_DESC desc[] = {
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 };
@@ -23,12 +26,10 @@ void SkyRenderer::setup(ID3D11Device* device, ShaderHandler& shaderHandler)
 	shaderHandler.setupPixelShader(device, SHADERKEY, L"SkymapPS.hlsl", "main");
 
 	ID3D11Texture2D* tex;
-	
-	D3D11_TEXTURE2D_DESC texDesc;
 
-	HRESULT hr = DirectX::CreateWICTextureFromFileEx(
+	HRESULT hr = DirectX::CreateDDSTextureFromFileEx(
 		device, 
-		L"../Resource/Textures/skybox_texture.jpg",
+		L"../Resource/Textures/skybox_texture.dds",
 		0, 
 		D3D11_USAGE_DEFAULT, 
 		D3D11_BIND_SHADER_RESOURCE,
@@ -43,17 +44,66 @@ void SkyRenderer::setup(ID3D11Device* device, ShaderHandler& shaderHandler)
 
 	tex->Release();
 
-	//TRYING WITHOUT THIS BECAUSE THAT WOULD BE SMART
-	//tex->GetDesc(&texDesc);
 
-	//D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
-	//ZeroMemory(&srvDesc, sizeof(D3D11_SHADER_RESOURCE_VIEW_DESC));
-	//srvDesc.Format = texDesc.Format;
-	//srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURECUBE;
-	//srvDesc.TextureCube.MipLevels = texDesc.MipLevels;
-	//srvDesc.TextureCube.MostDetailedMip = 0;
+	//this was very annoying, it better be right.
+	Vector3 cube[]
+	{
+		//front
+		Vector3(-1, -1, -1),
+		Vector3(-1, 1, -1),
+		Vector3(1, 1, -1),
+		
+		Vector3(1, 1, -1),
+		Vector3(1, -1, -1),
+		Vector3(-1, -1, -1),
 
+		//back
+		Vector3(-1, -1, 1),
+		Vector3(1, -1, 1),
+		Vector3(1, 1, 1),
 
+		Vector3(1, 1, 1),
+		Vector3(-1, 1, 1),
+		Vector3(-1, -1, 1),
+
+		//top
+		Vector3(-1, 1, -1),
+		Vector3(-1, 1, 1),
+		Vector3(1, 1, 1),
+
+		Vector3(1, 1, 1),
+		Vector3(1, 1, -1),
+		Vector3(-1, 1, -1),
+
+		//bottom
+		Vector3(-1, -1, -1),
+		Vector3(1, -1, -1),
+		Vector3(1, -1, 1),
+
+		Vector3(1, -1, 1),
+		Vector3(-1, -1, 1),
+		Vector3(-1, -1, -1),
+
+		//Left
+		Vector3(-1, -1, 1),
+		Vector3(-1, 1, 1),
+		Vector3(-1, 1, -1),
+
+		Vector3(-1, 1, -1),
+		Vector3(-1, -1, -1),
+		Vector3(-1, -1, 1),
+		
+		//Right
+		Vector3(1, -1, -1),
+		Vector3(1, 1, -1),
+		Vector3(1, 1, 1),
+		
+		Vector3(1, 1, 1),
+		Vector3(1, -1, 1),
+		Vector3(1, -1, -1),
+	};
+
+	//TODO: SAVE THIS IN A BUFFER AND ALL OTHER STUFFOKBYE
 }
 
 void SkyRenderer::render(ID3D11DeviceContext* context, ShaderHandler& shaderHandler, GAMESTATE const & state)
