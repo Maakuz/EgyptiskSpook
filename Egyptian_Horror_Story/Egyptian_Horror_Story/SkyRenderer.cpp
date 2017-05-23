59a0840a95bb48f8d02a35130adf7bf7c282c7fa
Markus Werdien <markuswerdien@hotmail.com>
2017-05-23 14:17:50 +0200

    Stole a skybox from google and put classes in folders

--- Egyptian_Horror_Story/Egyptian_Horror_Story/SkyRenderer.cpp
+++ Egyptian_Horror_Story/Egyptian_Horror_Story/SkyRenderer.cpp
@@ -8,10 +8,10 @@ SkyRenderer::~SkyRenderer()
 {
 }
 
-void SkyRenderer::setup(ID3D11Device * device, ShaderHandler & shaderHandler)
+void SkyRenderer::setup(ID3D11Device* device, ShaderHandler& shaderHandler)
 {
 }
 
-void SkyRenderer::render(ID3D11DeviceContext * context, ShaderHandler & shaderHandler, GAMESTATE const & state)
+void SkyRenderer::render(ID3D11DeviceContext* context, ShaderHandler& shaderHandler, GAMESTATE const & state)
 {
 }
